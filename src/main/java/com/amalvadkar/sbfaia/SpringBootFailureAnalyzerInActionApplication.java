18719886fc76df31670a98329ce636e17d0cdc18
Abhishek Malvadkar <abhi@dev.com>
2025-04-19 18:32:06 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/sbfaia/SpringBootFailureAnalyzerInActionApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.sbfaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFailureAnalyzerInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFailureAnalyzerInActionApplication.class, args);
+	}
+
+}
